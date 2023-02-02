33695b0d95480cabb66dbb54fce7ab199ba2e1f3
Carlos Sánchez <oceanrdn@gmail.com>
2023-02-02 20:26:32 +0100

    Generate passwords on button click. Cursor pointer on button

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let passwordOne = document.getElementById("passwordOne")
+let passwordTwo = document.getElementById("passwordTwo")
 
 function getRandomCharacter() {
     let randomChar = Math.floor(Math.random() * characters.length)
@@ -15,11 +17,9 @@ function generateRandomPassword() {
     return randomPassword
 }
 
-const generatedPasswordOne = generateRandomPassword()
-const generatedPasswordTwo = generateRandomPassword()
-
-let passwordOne = document.getElementById("passwordOne")
-let passwordTwo = document.getElementById("passwordTwo")
-
-passwordOne.textContent = generatedPasswordOne
-passwordTwo.textContent = generatedPasswordTwo
+function generateAndFillTwoRandomPasswords() {
+	const generatedPasswordOne = generateRandomPassword()
+	const generatedPasswordTwo = generateRandomPassword()
+	passwordOne.textContent = generatedPasswordOne
+	passwordTwo.textContent = generatedPasswordTwo
+}
